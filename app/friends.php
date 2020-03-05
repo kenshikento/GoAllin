1afeb5eb3c4f6d606220df0b728af4870b7094e7
Tien <tien-nguyen1@hotmail.co.uk>
2020-03-05 14:28:17 +0000

    tidy up

--- app/friends.php
+++ app/friends.php
@@ -6,21 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class friends extends Model
 {
-    //
     protected $fillable = ['user_ID', 'friendID', 'status'];
 
     public function user()
     {
         return $this->belongsTo('App\user','user_ID','id');
-
     }
-
 
     public function friends()
     {
-
         return $this->hasOne('app\chats','friend_ID');
-
     }
-
 }
