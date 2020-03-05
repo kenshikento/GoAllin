f577475bddc3b30f2be4d8c259045c91062b05c0
Tien <tien-nguyen1@hotmail.co.uk>
2020-03-05 14:29:40 +0000

    ty

--- app/messages.php
+++ app/messages.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class messages extends Model
 {
 	protected $fillable = ['status', 'user_ID', 'chat_ID','content'];
-	
-	public function chat() 
-    {
-        return $this->belongsTo('App\chats','chat_ID','id');
-    }
 
-    public function user()
-    {
-        return $this->belongsTo('App\user','user_ID','id');
-    }
+	public function chat() 
+	{
+		return $this->belongsTo('App\chats','chat_ID','id');
+	}
+
+        public function user()
+        {
+		return $this->belongsTo('App\user','user_ID','id');
+        }
 }
