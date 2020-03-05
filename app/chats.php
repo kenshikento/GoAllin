94fb7bedd8bf5ffbcca7841ba23837ad2f033f80
Tien <tien-nguyen1@hotmail.co.uk>
2020-03-05 14:26:57 +0000

    tidy up

--- app/chats.php
+++ app/chats.php
@@ -6,19 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class chats extends Model
 {
-
-
-
-   	public function chats(){
-
-        return $this->hasMany('App\messages','chat_ID');
-
+   	public function chats()
+   	{
+    	return $this->hasMany('App\messages','chat_ID');
     }
 
     public function friends()
-   
     {
         return $this->belongsTo('App\friends','friend_ID','id');
-
     }
 }
