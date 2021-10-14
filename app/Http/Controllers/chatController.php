80c1d1ccf408c90e66a3574dc22bb2a8ad324e6a
Tien <tien-nguyen1@hotmail.co.uk>
2021-10-14 01:10:39 +0100

    psudo revamp

--- app/Http/Controllers/chatController.php
+++ app/Http/Controllers/chatController.php
@@ -3,42 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\messages;
-use App\chats;
-use App\friends;
+use App\Messages;
+use App\Chats;
+use App\Friends;
 use App\User;
-use Auth;
-use DB;
 use Illuminate\Pagination\LengthAwarePaginator;
 
-class chatController extends Controller 
+class ChatController extends Controller 
 {
-	public function getMessages(Request $request)
+	public function index(ChatFormRequest $request)
 	{
         $chat = messages::with('chat.friends.user')->get();     
 
-        foreach ($chat as $chats)  
-        {       
-              // Data  
-              $info = $chats->chat->friends->friendID;  
-              // Friends name             
-              $friendname = $this->getName($info);
+        // need to use collection and use map
+        foreach ($chat as $chats) {       
+            // Data  
+            $info = $chats->chat->friends->friendID;  
+            // Friends name             
+            $friendname = $this->getName($info);
               
-              // User that is sending 
-              $userID=$chats->user_ID;
-              $username = $this->getName($userID);
+            // User that is sending 
+            $userID = $chats->user_ID;
+            $username = User::where('id', $id)->first();
 
-              $message[] = [
-                      'content'=>$chats->content,
-                      'created_at'=>$chats->created_at,
-                      'reciever'=>$friendname,
-                      'sender'=>$username
-                      ];
-
+            // use collection
+            $message[] = [
+                'content'=> $chats->content,
+                'created_at'=> $chats->created_at,
+                'reciever'=> $friendname,
+                'sender'=> $username
+            ];
         }
 
-        // Pagination code
+        // Pagination code // make global pagination
         $currentPage = LengthAwarePaginator::resolveCurrentPage();
+          
         $itemCollection = collect($message);
         $perPage = 5;
 
@@ -46,18 +45,7 @@ class chatController extends Controller
 
         $paginatedItems= new LengthAwarePaginator($currentPageItems , count($itemCollection), $perPage);
         $paginatedItems->setPath($request->url());
+
         return view('messageboard', ['message' => $paginatedItems]);
 	}
-
-
-    /**
-     * Gets the name
-     *
-     * @return string
-     */
-    public function getName(int $id) : string
-    {       
-	    $friendsname = User::where('id','=',$id)->get();  
-	    return $friendsname[0]->name;   
-    }
 }
